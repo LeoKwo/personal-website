c7bea2039f54e08509fd2a81e94ef8eadbf9861e
Ruikang (Leo) Guo <leokwok1997@hotmail.com>
2026-01-13 20:48:23 -0500

    update to chatbot

--- src/component/chatbot/chatbot.jsx
+++ src/component/chatbot/chatbot.jsx
@@ -1,53 +1,10 @@
 import React, { useState } from 'react';
-import { FaWindowMinimize, FaChevronUp } from "react-icons/fa";
+import { FaWindowMinimize } from "react-icons/fa";
 import { BsChatDotsFill } from "react-icons/bs";
 import './chatbot.scss';
-import axios from 'axios';
-import { useLanguage } from "../../translation/languageContext";
 
 const Chatbot = () => {
-    const { language, setLanguage, t } = useLanguage();
-    const [inputValue, setInputValue] = useState('');
-    const [messages, setMessages] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
     const [expanded, setExpanded] = useState(false);
-  
-    const handleChange = (e) => {
-        setInputValue(e.target.value);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        if (inputValue.trim() === '') return;
-  
-        setIsLoading(true); // Set loading state to true when the request starts
-  
-        try {
-            const url = "https://chattongyi-sawjcqgrkc.cn-hongkong.fcapp.run"
-            const payload = {
-                msg: inputValue,
-                thread_id: 1,
-            };
-
-            console.log(inputValue)
-
-            // Axios POST request
-            const response = await axios.post(url, payload, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-            console.log(response)
-            const botReply = response.data;
-        
-            setMessages([...messages, { text: inputValue, sender: 'user' }, { text: botReply, sender: 'bot' }]);
-            setInputValue('');
-        } catch (error) {
-            console.error('Error fetching chatbot response:', error);
-        } finally {
-            setIsLoading(false); // Set loading state to false after the request completes
-      }
-    };
 
     const toggleChatbot = () => {
         setExpanded(!expanded);
@@ -67,29 +24,12 @@ const Chatbot = () => {
                     <div className="chatbot-button" onClick={toggleChatbot}>
                         <FaWindowMinimize className="chatbot-button-icon"/>
                     </div>
-                    
-                    <div className="chatbot-messages">
-                        <span className="sender">{t('rkguo')}</span>
-                        <div className="message bot">{t('chatbotgreeting')}</div>
-                        {messages.map((message, index) => (
-                            <>
-                                {
-                                    message.sender === 'user' ? 
-                                    <span className="sender human">{t('chatbotuser')}</span> :
-                                    <span className="sender">{t('rkguo')}</span>
-                                }
-                                <div key={index} className={`message ${message.sender}`}>
-                                    {message.text}
-                                </div>
-                            </>
-                        ))}
-                    </div>
-                    <form className="chatbot-form" onSubmit={handleSubmit}>
-                        <input className="chatbot-form-input" type="text" value={inputValue} onChange={handleChange} placeholder={t('chatbotprompt')} />
-                        <button className="chatbot-form-button rn-button-style--2 btn-solid" type="submit">{<FaChevronUp/>}</button>
-                    </form>
-                    {isLoading && <div className="loading-indicator">{t('chatbotspinner')}</div>}
-
+                    <iframe
+                        title='GuoGenius'
+                        src="https://bot.rkguo.xyz/?embed=true"
+                        height="550"
+                        style={{"width":"350px", "border":"none"}}
+                    ></iframe>
                 </div>
             }
         </div>
